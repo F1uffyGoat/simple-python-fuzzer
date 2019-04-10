2ad22f3ff4bfd53773bf65b8a63a16e0b887baba
F1uffyGoat <43916815+F1uffyGoat@users.noreply.github.com>
2019-04-10 16:47:29 +0200

    Create fuzz.py
    Very simple python fuzzer

---
+++ fuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+
+buffer=["A"]
+counter=100
+while len(buffer) <= 10:
+        buffer.append("A"*counter)
+        counter=counter+100 # Increment by 100
+
+for string in buffer:
+        print "Fuzzing App with %s bytes" % len(string)
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connect=s.connect(('127.0.0.1', 9999))
+        s.recv(1024)
+        s.send(string + '\r\n')
+        s.close()
